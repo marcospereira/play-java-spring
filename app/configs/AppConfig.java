91e7edd5853d8a1caa24e67462c2178976ab3f3b
Marcos Pereira <marcos.silva@gmail.com>
2017-05-22 14:25:08 -0300

    Enable Spring cache
    We then need to enable Spring cache by:
    
    1. Adding the @EnableCaching annotation to our AppConfig. This is
    described at Spring documentation here:
    
    https://docs.spring.io/spring/docs/4.1.x/spring-framework-reference/html/cache.html#cache-annotation-enable
    
    2. Create the Spring `cacheManager` bean. Here we are reading the cache
    configuration from `ehcache.xml` which should be in `conf` directory of your
    application.

--- app/configs/AppConfig.java
+++ app/configs/AppConfig.java
@@ -1,10 +1,39 @@
 package configs;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
 
 @Configuration
+@EnableCaching
 @ComponentScan({"controllers", "services"})
-public class AppConfig {
+public class AppConfig extends CachingConfigurerSupport {
 
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        return new EhCacheCacheManager(ehcache());
+    }
+
+    @Bean
+    public net.sf.ehcache.CacheManager ehcache() {
+        try {
+            Resource config = new ClassPathResource("ehcache.xml");
+            net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.create(config.getInputStream());
+            for (String s : cacheManager.getCacheNames()) {
+                System.out.println("Cache => " + s);
+            }
+            return cacheManager;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
